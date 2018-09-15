e7696a3b097bd5bb39805912a3bf8bcf2c5c979e
Sara Hunter <34786124+sarashunter@users.noreply.github.com>
2018-09-15 13:33:21 -0400

    Add bidding function

--- greatbay.js
+++ greatbay.js
@@ -44,7 +44,10 @@ function mainMenu() {
         console.log(response.action);
         if (response.action === 'POST AN ITEM') {
             postItem();
-        } else if (response.action === 'EXIT') {
+        } else if (response.action === 'BID ON AN ITEM') {
+            bid();
+        }
+        else if (response.action === 'EXIT') {
             connection.end();
         }
     });
@@ -70,4 +73,76 @@ function postItem() {
             })
         mainMenu();
     })
+}
+
+function bid() {
+    connection.query("SELECT * FROM items", function (err, res) {
+        if (err) throw err;
+        var choicesArray = [];
+
+        console.log(res);
+        console.log(`Item Number    Item   Highest Bidder  Highest Bid`);
+        res.forEach(function (element) {
+            choicesArray.push(element.id.toString());
+            console.log(`${element.id}  ${element.item_name}       ${element.highest_bidder}       ${element.highest_bid}`);
+        })
+
+        inquirer.prompt([
+            {
+                type: 'list',
+                choices: choicesArray,
+                message: 'What is the item number you would like to bid on?',
+                name: 'itemNumber'
+            },
+            {
+                type: 'input',
+                message: 'How much would you like to bid?',
+                name: 'bidAmt'
+            }
+        ]).then(function (response) {
+            console.log("in here?" + response.itemNumber);
+            //check current bid.  Compare.
+            connection.query("SELECT * FROM items WHERE ?",
+            {
+                id: response.itemNumber
+            },
+            function(err, res){
+                if (err) throw err;
+                console.log(res);
+                console.log(res[0].highest_bid);
+                if(parseFloat(response.bidAmt) > parseFloat(res[0].highest_bid)){
+                    console.log("You have the new highest bid");
+                    updateHighBid(response.itemNumber, response.bidAmt);
+                }else{
+                    console.log("That bid is not high enough");
+                    mainMenu();
+                }
+            });
+
+            //if it's bigger, update
+
+            //else console.log
+            // mainMenu();
+        });
+
+    })
+
+}
+
+function updateHighBid(id, bid){
+    connection.query("UPDATE items SET ? WHERE ?",
+    [
+        {
+            highest_bid: bid
+        },
+        {
+            id: id
+        }
+    ],
+    function(err, res){
+        if (err) throw err;
+        console.log("Updated bid");
+        mainMenu();
+    }
+    )
 }
